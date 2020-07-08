ef3d8e348cac366c13c26b83c75f90b9b4bf1ec0
Lenniet M. Coello <Lenniet_M_Coello@ryder.com>
2020-07-08 12:39:28 -0400

    importing_jupyter_notebooks

--- code/missing_class.py
+++ code/missing_class.py
@@ -5,16 +5,31 @@ from scipy import stats
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-dataset = pd.read_csv('archive/uvs_report.csv', sep=',', error_bad_lines=False, low_memory=False)
-dataset.shape
+dataset = pd.read_csv('archive/amp.csv', sep=',', error_bad_lines=False, low_memory=False, parse_dates=[18,29,31,32,37,41,42,44])
+#dataset.shape
 dataset.head()
-dataset.describe()
+#dataset.describe()
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+#data types
+type(dataset)
+dataset.dtypes
+
+#count Nulls per column
+percent_missing = dataset.isnull().sum() * 100 / len(dataset)
+missing_value_df = pd.DataFrame({'column_name': dataset.columns, 'percent_missing': percent_missing})
 
 #clean up missing data
 
-#univariable analysis
+#univariable analysis (Column Level)
+Index_keep = missing_value_df.query('percent_missing < 75').index.tolist() 
+column_keep = Index_keep
+dataset = dataset[column_keep]
+dataset.columns
 
-#multivariable analysis (MCAR, MAR, NMAR)
+#multivariable analysis (MCAR, MAR, NMAR) (Row Level)
 
 #Pairwise deletion
 
